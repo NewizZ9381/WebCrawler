efd652e16ea524624294050f1b31b268a2bcf51e
NewizZ9381 <newkmitl47@gmail.com>
2017-09-26 20:55:27 +0700

    Add Procedure

--- WebCrawler.py
+++ WebCrawler.py
@@ -11,10 +11,17 @@
 # and therefore still prints the same thing.
 
 # page = contents of a web page
+
+def get_next_link(page):
+    start_link = page.find('<a href=')
+    start_quote = page.find('"',start_link)
+    end_quote = page.find('"',start_quote+1)
+    url = page[start_quote+1:end_quote]
+    return url, end_quote
+
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
-start_link = page.find('<a href=')
-start_quote = page.find('"',start_link)
-end_quote = page.find('"',start_quote+1)
-url = page[start_quote+1:end_quote]
+url, end_quote = get_next_link(page)
+
 print (url)
+print (end_quote)
